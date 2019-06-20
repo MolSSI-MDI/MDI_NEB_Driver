d1de2be3e54b2f109a56ff2cf85777e75e65ed0b
Samuel Ellis <sjellis@vt.edu>
2019-06-20 13:29:42 -0400

    Added the testing directory with sample files.

---
+++ tests/NEB/testing.sh
@@ -0,0 +1,30 @@
+#location of required codes
+DRIVER_LOC=$(cat ../locations/MDI_NEB_Driver)
+LAMMPS_LOC=$(cat ../locations/LAMMPS)
+NENGINES=$1
+SPRING_CONST=$2
+ENERGY_THRESHOLD=$3
+FORCE_THRESHOLD=$4
+
+#remove old files
+if [ -d work ]; then 
+  rm -r work
+fi
+
+#create work directory
+cp -r data work
+cd work
+
+#launch QE
+#${QE_LOC} -mdi "-role ENGINE -name QM -method TCP -port 8021 -hostname localhost" -in qe.in > qe.out &
+
+for i in $(seq 1 ${NENGINES})
+do
+	echo "Iteration $i"
+	#launch LAMMPS
+	${LAMMPS_LOC} -mdi "-role ENGINE -name MM$i -method TCP -port 8021 -hostname localhost" -in $i.in > $i.out &
+done
+#launch drive
+${DRIVER_LOC} -mdi "-role DRIVER -name driver -method TCP -port 8021" -engines ${NENGINES} -spring ${SPRING_CONST} -energy_threshold ${ENERGY_THRESHOLD} -force_threshold ${FORCE_THRESHOLD}  &
+
+wait
